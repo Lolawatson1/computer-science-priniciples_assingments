6cfb93d20f5a4551fcb549e2242ba8bdce13b84e
Lolawatson1 <lola.watson@ucas-edu.net>
2024-09-07 21:38:39 +0000

    wrote a variable and printed it

---
+++ vp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    char name [] = "lola";
+    printf("%s,", name);
+    char number [] = "7";
+    printf(" %s,",number);
+    char numbr [] = "37";
+    printf(" %s,", numbr);
+    char breakfast [] = "waffles";
+    printf(" %s,", breakfast);
+    char color [] = "orange";
+    printf(" %s,", color);
+    char school [] = "ucas";
+    printf(" %s,", school);
+    char year [] = "2024";
+    printf(" %s,", year);
+    char eyes [] = "green";
+    printf(" %s,", eyes);
+    char age [] = "14";
+    printf(" %s,", age);
+    char subject [] = "math";
+    printf(" %s,", subject);
+ return 0;
+}
---
+++ vp.py
@@ -0,0 +1,23 @@
+import math
+
+
+name = "lola"
+print(name)
+number = "7" 
+print(number)
+numbr = "37"
+print(numbr)
+breakfast = "waffles"
+print(breakfast)
+color = "orange"
+print(color)
+school = "ucas"
+print(school)
+year = "2024"
+print(year)
+eye = "green"
+print(eye)
+age = "14"
+print(age)
+subject = "science"
+print(subject)
