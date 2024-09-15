734847ea138a79cdca0b8d8bd887bcfd92eff93e
Lolawatson1 <lola.watson@ucas-edu.net>
2024-09-15 18:29:47 +0000

    wrote and printed expressions

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -1,8 +1,23 @@
-#include <studio.h>
-#include <math.h>
+#include <stdio.h>
 
 int main(void){
-    int number = (int) pow(4,3)
-    printf("%d",number);
+    int numberone = 7-24/8*4+6;
+    printf("%d\n",numberone);
+    int numbertwo = 18/3-7+2*5;
+    printf("%d\n",numbertwo);
+    int numberthree = 6*4/12+72/8-9;
+    printf("%d\n",numberthree);
+    int numberfour = ((17-6/2)+4*3);
+    printf("%d\n",numberfour);
+    int numberfive = -2*(1*4-2/2)+(6+2-3);
+    printf("%d\n",numberfive);
+    int numbersix = -1*((3-4*7)/5)-2*24/6;
+    printf("%d\n",numbersix);
+    int numberseven = (3*pow(5,2)/15)-(5-pow(2,2));
+    printf("%d\n",numberseven);
+    int numbereight = (pow(1,4)*pow(2,2)+pow(3,3))-(pow(2,5)/4);
+    printf("%d\n",numbereight);
+    int numbernine = (pow(22/2-2*5,2)+pow(4-6/6,2));
+    printf("%d\n",numbernine);
     return 0;
 }
