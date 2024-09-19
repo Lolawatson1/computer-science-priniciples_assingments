3df2eeff3598c9d836e0ea80f13d9823ee77037a
Lolawatson1 <lola.watson@ucas-edu.net>
2024-09-19 02:19:01 +0000

    made the code for python

--- strings/namedecorator.c
+++ strings/namedecorator.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-char name[30]  
+char name[50]  
 printf("please tell me your name: \n");
-scanf("%s", name);
+scanf("%49s", name);
+printf("<<< %s >>>\n", name);
+printf("((( %s )))\n", name);
+printf("--- %s ---\n", name);
+printf(":) :) %s :) :)\n", name);
+printf("### %s ###\n", name);
+printf("+++ %s +++\n", name);
+printf("~~~ %s ~~~\n", name);
+printf("=== %s ===\n", name);
 return 0;
 }
