495e1ec644152a952050e3e13485f49dd399d56d
Lolawatson1 <lola.watson@ucas-edu.net>
2024-09-20 19:28:46 +0000

    made silly sentances in python

---
+++ strings/sillysentances.py
@@ -0,0 +1,10 @@
+animal = input("Name an animal: ")
+place = input("Name a place: ")
+verb = input("Name a verb: ")
+print("The", animal)
+print(" went to the ", place)
+print(" and ", verb)
+print(" with the clouds.")
+
+
+
