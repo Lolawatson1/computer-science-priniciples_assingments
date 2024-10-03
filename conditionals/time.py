3c82cb36eb0a8cc3063471f5559c3ae9384603e2
Lolawatson1 <lola.watson@ucas-edu.net>
2024-10-03 19:37:36 +0000

    alot of things

--- conditionals/time.py
+++ conditionals/time.py
@@ -3,3 +3,14 @@ import datetime
 currenttime = datetime.datetime.now()
 
 print(currenttime.hour)
+
+hour = 13
+
+if hour < 12:
+    print("Good Morning")
+elif hour < 15:
+    print("Good Afternoon")
+else:
+    print("Good Afternoon")
+
+
---
+++ time_extra.py
@@ -0,0 +1,24 @@
+
+if currenttime.hour < 12:
+   print("Good Morning")
+elif currenttime.hour < 15:
+    print("Good Afternoon")
+else currenttime.hour 
+    print("Good Evening")
+
+
+
+    import datetime
+
+currenttime = datetime.datetime.now()
+
+print(currenttime.hour)
+
+hour = input("Tell me the hour in military time\n")
+
+if hour < 12:
+    print("Good Morning")
+elif hour < 15:
+    print("Good Afternoon")
+else:
+    print("Good Afternoon")
