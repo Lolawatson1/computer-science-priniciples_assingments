82d70c776156761d7acd486ebb5f6d46d00dd6d4
Lolawatson1 <lola.watson@ucas-edu.net>
2024-10-11 19:06:26 +0000

    fixed code

--- loops/family_loop.py
+++ loops/family_loop.py
@@ -1,4 +1,5 @@
-silbings = ["Max", "Xander", "Barrett", "Jacq","Lola", "William", "Oliver"]
-while (silbings):
-    print(silbings)
+siblings = ["Max", "Xander", "Barrett", "Jacq","Lola", "William", "Oliver"]
+for (sib) in siblings:
+    print(sib)
     
+
